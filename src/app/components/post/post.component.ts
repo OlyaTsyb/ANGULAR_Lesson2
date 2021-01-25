4e982b30672f12198b1c6431d1a04f050adc8b06
Olya <olyabusol1605@gmail.com>
2021-01-25 14:33:35 +0200

    AngularLesson_2_do_commit_to_EventEmitter

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -12,7 +12,7 @@ export class PostComponent implements OnInit {
   post: Post;
 
   @Output()
-  bubbleUpPost = new EventEmitter();
+  bubbleUpPost = new EventEmitter<Post>();
 
   constructor() {
   }
